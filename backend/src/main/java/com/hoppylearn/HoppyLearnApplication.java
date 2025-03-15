fd8125493f92d3c454407a5de8acba2a7812f866
minsoen.park <minseonp123@gmail.com>
2025-03-15 18:56:13 -0500

    initialize spring boot project using spring initializr

---
+++ backend/src/main/java/com/hoppylearn/HoppyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.hoppylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppyLearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hoppylearn/HoppyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoppylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
