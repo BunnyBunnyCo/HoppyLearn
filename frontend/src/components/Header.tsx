cf4550831920a32579c2af31189f0f406b234372
remy-z <remyszach@gmail.com>
2025-03-21 13:52:52 -0700

    clean up extraneous code

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,11 @@
 import * as types from "../types";
-import NavButton from "./NavButton";
-import { useContext } from "react";
-import { CurrentDeckContext } from "../contexts/CurrentDeckContextProvider";
 
 const Header: React.FC<types.HeaderProps> = ({ navButtons, title }) => {
-  const { currentDeck } = useContext(CurrentDeckContext);
   return (
     <div className="header">
       <h1 className="header-title">{title}</h1>
       <div className="header-buttons">
-        {navButtons.map((button, index) => button)}
+        {navButtons.map((button, _) => button)}
       </div>
     </div>
   );
