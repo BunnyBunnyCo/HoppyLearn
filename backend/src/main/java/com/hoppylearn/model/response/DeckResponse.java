d6032fe69cc7963908eb652234bd3d7fbab5369a
minsoen.park <minseonp123@gmail.com>
2025-03-20 19:10:30 -0500

    create mock request and response. enable actuator

---
+++ backend/src/main/java/com/hoppylearn/model/response/DeckResponse.java
@@ -0,0 +1,15 @@
+package com.hoppylearn.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeckResponse {
+    private int id;
+    private String deckName;
+}
