501ee2fa7bf2646c13c634f7f5a6d215d12555ea
remy-z <remyszach@gmail.com>
2025-03-21 21:59:56 -0700

    Change Style; Add input handling in HomeDeckInput

--- frontend/src/components/HomeDeck.tsx
+++ frontend/src/components/HomeDeck.tsx
@@ -1,5 +1,31 @@
-const HomeDeck: React.FC = () => {
-  return <></>;
+import { Deck } from "../types";
+import NavButton from "./NavButton";
+import edit from "../assets/edit.jpg";
+import book from "../assets/book.svg";
+
+interface HomeDeckProps {
+  deck: Deck;
+}
+const HomeDeck: React.FC<HomeDeckProps> = ({ deck }) => {
+  return (
+    <li key={deck.id} className="list-deck">
+      <span className="text">{deck.name}</span>
+      <div className="nav-button-container">
+        <NavButton
+          icon={book}
+          text=""
+          alt="Go Study"
+          destination={`/study/${deck.id}`}
+        />
+        <NavButton
+          icon={edit}
+          text=""
+          alt="Go Edit"
+          destination={`/edit/${deck.id}`}
+        />
+      </div>
+    </li>
+  );
 };
 
 export default HomeDeck;
