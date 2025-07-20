b1f6e704680c451b98d97143808a888d7b792e52
remy-z <remyszach@gmail.com>
2025-07-19 23:34:50 -0700

    Local Storage for editing cards

--- frontend/src/pages/DeckEditor/EditCardInput.tsx
+++ frontend/src/pages/DeckEditor/EditCardInput.tsx
@@ -1,15 +1,24 @@
 import React, { useContext } from "react";
 import { EditDeckContext } from "../../contexts/EditDeckContextProvider";
 import * as types from "../../types";
+import * as util from "../../utils/localStoreCalls";
 import styles from "./styles/EditCard.module.css";
+import { CurrentDeckContext } from "../../contexts/CurrentDeckContextProvider";
+import { DecksContext } from "../../contexts/DecksContextProvider";
 
 interface EditCardInputProps {
   deck: types.Deck;
   resetDecks: (updatedDeck: types.Deck) => void;
+  resetCards: (newCard: types.Card) => void;
 }
 
-const EditCardInput: React.FC<EditCardInputProps> = ({ deck, resetDecks }) => {
+const EditCardInput: React.FC<EditCardInputProps> = ({
+  resetCards,
+  resetDecks,
+}) => {
   const { inputCard, setCard } = useContext(EditDeckContext);
+  const { decks } = useContext(DecksContext);
+  const { currentDeck } = useContext(CurrentDeckContext);
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
@@ -20,10 +29,15 @@ const EditCardInput: React.FC<EditCardInputProps> = ({ deck, resetDecks }) => {
   }
 
   function addCard() {
+    const deck = decks.get(currentDeck.id);
+    if (!deck) {
+      return;
+    }
     if (inputCard.front.trim() !== "" && inputCard.back.trim() !== "") {
-      const newCard = types.createCard(deck.id, inputCard);
-      deck.cards.push(newCard);
+      const newCard = util.createCard(deck.id, inputCard);
+      deck.cards.push(newCard.id);
       resetDecks(deck);
+      resetCards(newCard);
       setCard(types.createInputCard());
     }
   }
--- frontend/src/pages/DeckEditor/EditCardList.tsx
+++ frontend/src/pages/DeckEditor/EditCardList.tsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as types from "../../types";
 import EditCard from "./EditCard";
 import EditCardInput from "./EditCardInput";
 import styles from "./styles/EditCardList.module.css";
+import * as util from "../../utils/localStoreCalls";
+import { CurrentDeckContext } from "../../contexts/CurrentDeckContextProvider";
+import { DecksContext } from "../../contexts/DecksContextProvider";
 
 interface EditCardListProps {
-  deck: types.Deck;
   resetDecks: (updatedDeck: types.Deck) => void;
+  resetCards: (newCard: types.Card) => void;
+  cards: Map<number, types.Card>;
+  setCards: React.Dispatch<React.SetStateAction<Map<number, types.Card>>>;
 }
 
-const EditCardList: React.FC<EditCardListProps> = ({ deck, resetDecks }) => {
+const EditCardList: React.FC<EditCardListProps> = ({
+  resetDecks,
+  resetCards,
+  cards,
+}) => {
+  const { currentDeck } = useContext(CurrentDeckContext);
+  const { decks } = useContext(DecksContext);
+  const deck = decks.get(currentDeck.id);
+  if (!deck) {
+    return null;
+  }
   return (
     <ol className={styles.editCardList}>
-      {deck.cards.map((card, index) => (
+      {deck.cards.map((cardID, index) => (
         <EditCard
-          key={card.id}
-          card={card}
+          key={cardID}
+          card={
+            cards.get(cardID) ||
+            util.createCard(deck.id, { front: "", back: "" })
+          }
           deck={deck}
           index={index}
           resetDecks={resetDecks}
         />
       ))}
-      <EditCardInput deck={deck} resetDecks={resetDecks} />
+      <EditCardInput
+        deck={deck}
+        resetCards={resetCards}
+        resetDecks={resetDecks}
+      />
     </ol>
   );
 };
