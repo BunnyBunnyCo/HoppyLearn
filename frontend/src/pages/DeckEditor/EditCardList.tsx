7f8be6d2999c9127d289c4c878d83e10c27b9a15
remy-z <remyszach@gmail.com>
2025-07-19 20:43:22 -0700

    Make header consistent across pages

--- frontend/src/pages/DeckEditor/EditCardList.tsx
+++ frontend/src/pages/DeckEditor/EditCardList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as types from "../../types";
 import EditCard from "./EditCard";
 import EditCardInput from "./EditCardInput";
+import styles from "./styles/EditCardList.module.css";
 
 interface EditCardListProps {
   deck: types.Deck;
@@ -10,7 +11,7 @@ interface EditCardListProps {
 
 const EditCardList: React.FC<EditCardListProps> = ({ deck, resetDecks }) => {
   return (
-    <ol>
+    <ol className={styles.editCardList}>
       {deck.cards.map((card, index) => (
         <EditCard
           key={card.id}
