ed3b9926b809c60eb0eba786fa6a3b4287f7bf4e
remy-z <remyszach@gmail.com>
2025-07-22 23:00:11 -0700

    Update Controller and Test to use data and service layers

--- backend/src/main/java/com/hoppylearn/model/entity/Deck.java
+++ backend/src/main/java/com/hoppylearn/model/entity/Deck.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,6 +15,7 @@ import lombok.ToString;
 public class Deck {
     private Long id;
     private String deckName;
+    private List<Long> cardIds;
 
     // Custom constructor for common use case
     public Deck(String deckName) {
